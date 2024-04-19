6b9ae7fb00c0daea2bd4b2d68d076bcb815a4a7c
justtns <justinloh20@gmail.com>
2024-04-19 19:20:33 +0800

    branch dao

--- src/main/java/domain/models/Admin.java
+++ src/main/java/domain/models/Admin.java
@@ -5,12 +5,11 @@ public class Admin extends Staff {
 
     // Constructor
     public Admin(String name, String password, String gender, int age) {
-        super(name,  password, Role.Admin, gender, age, "All Branches");
+        super(name,  password, Role.Admin, gender, age, "NA");
     }
 
     public Admin(String name,String id, String password, String gender, int age) {
-        super(name,id,  password, Role.Admin, gender, age, "All Branches");
+        super(name,id,  password, Role.Admin, gender, age, "NA");
     }
-
-
+    
 }
--- src/main/java/domain/models/BranchManager.java
+++ src/main/java/domain/models/BranchManager.java
@@ -4,7 +4,6 @@ import main.java.domain.types.Role;
 public class BranchManager extends Staff {
     // Additional attributes or methods specific to BranchManager can be added here
 
-
     public BranchManager(String name,  String password,  String gender, int age, String branch) {
         super(name,  password, Role.Manager, gender, age, branch);
     }
