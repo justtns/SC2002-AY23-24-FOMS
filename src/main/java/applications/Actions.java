03094c2e6f208d8e4755f1a48aa7f7987f94ff15
justtns <justinloh20@gmail.com>
2024-04-20 23:19:07 +0800

    renamed application to action

--- src/main/java/applications/Actions.java
+++ src/main/java/applications/Actions.java
@@ -2,6 +2,6 @@ package main.java.applications;
 
 import main.java.utils.loggers.CustomerSession;
 
-public interface Application {
+public interface Actions {
     public void execute(CustomerSession session);
 }
--- src/main/java/applications/CustomerOrderingAction.java
+++ src/main/java/applications/CustomerOrderingAction.java
@@ -3,7 +3,7 @@ package main.java.applications;
 import main.java.boundaries.CustomerOrderingForm;
 import main.java.utils.loggers.CustomerSession;
 
-public class CustomerOrderingApplication implements Application{
+public class CustomerOrderingAction implements Actions{
 
     public void execute(CustomerSession session){
         CustomerOrderingForm orderForm = new CustomerOrderingForm(session);
