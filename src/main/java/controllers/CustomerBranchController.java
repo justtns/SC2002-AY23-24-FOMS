aca7793d571cacf73c48f808de1a2bae3bd882bf
justtns <justinloh20@gmail.com>
2024-04-21 17:42:35 +0800

    CustomerBranchController

---
+++ src/main/java/controllers/CustomerBranchController.java
@@ -0,0 +1,24 @@
+package main.java.controllers;
+
+import java.util.List;
+import main.java.daos.BranchDAO;
+import main.java.models.Branch;
+
+public class CustomerBranchController {
+    private Branch branchDAO;
+
+    public CustomerBranchController(Branch branchDAO){
+        this.branchDAO = branchDAO;
+    }
+
+    public List<Branch> getBranches(){
+        return branchDAO.getElements;
+    }
+
+    public Boolean verifyBranch(String branchName){
+        if (branchDAO.findElement(branchName) == null){
+            return false;
+        }
+        return true;
+    }
+}
