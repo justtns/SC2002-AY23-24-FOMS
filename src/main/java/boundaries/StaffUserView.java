38aa8010019e282368936b71db9a58cef6f88a7f
Ian Boo <ianboo.jy@gmail.com>
2024-04-22 04:45:43 +0900

    Update StaffUserView.java

--- src/main/java/boundaries/StaffUserView.java
+++ src/main/java/boundaries/StaffUserView.java
@@ -1,8 +1,7 @@
 package main.java.boundaries;
 
-import java.util.Scanner;
 import main.java.utils.loggers.StaffSession;
 
 public interface StaffUserView {
-    public void execute(staffSession session);
+    public void execute(StaffSession session);
 }
