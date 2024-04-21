4b8d6441258de601ec0a488f9c1540a307beb78a
Ian Boo <ianboo.jy@gmail.com>
2024-04-22 00:28:16 +0900

    Create a StaffAppRunner

---
+++ src/test/java/Applications/StaffAppRunner.java
@@ -0,0 +1,11 @@
+package test.java.Applications;
+
+
+import main.java.boundaries.StaffApp;
+
+public class StaffAppRunner {
+    public static void main(String[] args) {
+        StaffApp staffApp = new StaffApp();
+        staffApp.execute();
+    }
+}
