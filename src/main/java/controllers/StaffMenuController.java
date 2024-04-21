8978532f2cc2b8ff84014921b5c08580aec20027
Rachel <rachelkjhh@gmail.com>
2024-04-21 18:33:11 +0800

    menucontroller added and menuitem updated

---
+++ src/main/java/controllers/StaffMenuController.java
@@ -0,0 +1,59 @@
+package main.java.controllers;
+
+import main.java.daos.MenuDAO;
+import main.java.models.MenuItem;
+import java.util.List;
+
+public class StaffMenuController {
+    private MenuDAO menuDAO;
+
+    public StaffMenuController(MenuDAO menuDAO) {
+        this.menuDAO = menuDAO;
+    }
+
+    public List<MenuItem> getItems(){
+        return menuDAO.getElements();
+    }
+
+    public void addMenuItem(MenuItem item) {
+        if (menuDAO.findElement(item.getName(), item.getBranch()) == null) {
+            menuDAO.addElement(item);
+            System.out.println("Menu item added: " + item.getName());
+        } else {
+            System.err.println("Menu item already exists in branch: " + item.getBranch());
+        }
+    }
+
+    public void editMenuItem(MenuItem oldItem, MenuItem updatedItem) {
+
+        MenuItem currentItem = menuDAO.findElement(oldItem.getName(), oldItem.getBranch());
+        if (currentItem != null) {
+            menuDAO.updateElement(currentItem, updatedItem);
+            System.out.println("Menu item updated: " + updatedItem.getName());
+        } else {
+            System.err.println("Menu item with name " + oldItem.getName() + " not found in branch: " + oldItem.getBranch());
+        }
+    }
+
+    public void removeMenuItem(MenuItem item) {
+        MenuItem currentItem = menuDAO.findElement(item.getName(), item.getBranch());
+        if (currentItem != null) {
+            menuDAO.removeElement(currentItem.getName(), currentItem.getBranch());
+            System.out.println("Menu item removed: " + item.getName());
+        } else {
+            System.err.println("Menu item with name " + item.getName() + " not found in branch: " + item.getBranch());
+        }
+    }
+
+    public void changeAvailability(MenuItem item, boolean availability) {
+        MenuItem currentItem = menuDAO.findElement(item.getName(), item.getBranch());
+        if (currentItem != null) {
+            currentItem.setAvailable(availability);
+            System.out.println("Menu item availability changed: " + item.getName());
+        } else {
+            System.err.println("Menu item with name " + item.getName() + " not found in branch: " + item.getBranch());
+        }
+    }
+}
+
+
--- src/main/java/models/MenuItem.java
+++ src/main/java/models/MenuItem.java
@@ -2,30 +2,67 @@ package main.java.models;
 
 public class MenuItem {
     private String name;
-    private String category;
-    private String branch;
     private double price;
+    private String branch;
+    private String category;
+    private boolean isAvailable; 
 
     public MenuItem(String name, String category, String branch, double price) {
         this.name = name;
-        this.category = category;
-        this.branch = branch;
         this.price = price;
+        this.branch = branch;
+        this.category = category;
+        this.isAvailable = true; 
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCategory() {
-        return category;
+    public double getPrice() {
+        return price;
     }
 
     public String getBranch() {
         return branch;
     }
 
-    public double getPrice() {
-        return price;
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", branch='" + branch + '\'' +
+                ", category='" + category + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
     }
 }
