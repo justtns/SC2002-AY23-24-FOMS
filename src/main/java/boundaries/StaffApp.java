96a514c7389edcb3e0a7cd46f16f9383f9ecac9c
xndyyy <163510239+xndyyy@users.noreply.github.com>
2024-04-21 16:36:24 +0800

    updated roles

---
+++ src/main/java/boundaries/StaffApp.java
@@ -0,0 +1,18 @@
+package main.java.boundaries;
+
+import main.java.utils.types.Role;
+import main.java.utils.types.staffRole;
+
+public class StaffApp implements AppDisplay {
+    private Role loginRole;
+    private staffRole staffRole;
+
+    @Override
+    public Role enterRole() {
+        // undone
+    }
+
+    public void executeLogin() {
+        // undone
+    }
+}
--- src/main/java/utils/types/LoginRole.java
+++ src/main/java/utils/types/LoginRole.java
@@ -1,5 +1,5 @@
 package main.java.utils.types;
 
-public enum Role {
+public enum LoginRole {
     Customer, Staff
 }
