ef2fdfe8f07b8f9d45a80d9ccd3639c82a278c2c
Ian Boo <ianboo.jy@gmail.com>
2024-04-22 04:45:46 +0900

    Update StaffView.java

--- src/main/java/boundaries/StaffView.java
+++ src/main/java/boundaries/StaffView.java
@@ -1,18 +1,18 @@
 package main.java.boundaries;
 
-import main.java.utils.loggers.CustomerSession;
-import main.java.boundaries.StaffUserView;
+import main.java.utils.loggers.StaffSession;
+import main.java.actions.StaffActions;
+import main.java.actions.StaffOrderAction;
 // import staff actions when done
 import main.java.utils.ScannerProvider;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 
 public class StaffView implements StaffUserView{
     public void execute(StaffSession session){
         Scanner scanner = new Scanner(System.in);
         StaffActions action; // undone
-        System.out.println("User ID:" + session.getUserId());
+        System.out.println("User ID:" + session.getStaffUserID());
 
         action = new StaffOrderAction(); //havent implement
         action.execute(session, scanner);
