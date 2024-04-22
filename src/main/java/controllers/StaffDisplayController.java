a07219f237a2dc3495d3d6c3608f4b2d37c2839e
Ian Boo <ianboo.jy@gmail.com>
2024-04-22 14:28:09 +0900

    Update StaffDisplayController.java

--- src/main/java/controllers/StaffDisplayController.java
+++ src/main/java/controllers/StaffDisplayController.java
@@ -1,17 +1,29 @@
 package main.java.controllers;
 
-import main.java.models.Order;
-import main.java.daos.OrderDAO;
 import main.java.models.Staff;
-import main.java.utils.types.OrderStatus;
+import java.util.List;
+
+
 import main.java.daos.StaffDAO;
 
 public class StaffDisplayController {
     private StaffDAO staffDAO;
+    private String branch;
 
-    public StaffDisplayController(StaffDAO staffDAO){
+    public StaffDisplayController(StaffDAO staffDAO, String branch){
         this.staffDAO = staffDAO;
+        this.branch = branch;
     }
 
+    public void displayStaffList(){  // implement display using STAFF DAO
+        List<Staff> staffList = staffDAO.getElements();
+        // Print the staff details who belong to the 'branch'
+        System.out.println("Staff list for branch: " + branch);
+        for (Staff staff : staffList) {
+            if (staff.getBranch().equals(branch)) {
+                System.out.println(staff);
+            }
+        }
+    }
 
 }
