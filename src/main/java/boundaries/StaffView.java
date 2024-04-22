c44e42871c2377537240fcb598eb65af839ce234
xndyyy <163510239+xndyyy@users.noreply.github.com>
2024-04-22 16:21:19 +0800

    updated staffview and stafforderform

--- src/main/java/boundaries/StaffView.java
+++ src/main/java/boundaries/StaffView.java
@@ -12,9 +12,43 @@ public class StaffView implements StaffUserView{
         Scanner scanner = new Scanner(System.in);
         StaffActions action;
         System.out.println("User ID:" + session.getStaffUserID());
+        int choice;
 
-        action = new StaffOrderAction();
-        action.execute(session, scanner);
+        System.out.println("-------------------------------------------------------------------\n" +
+                    "-----------------------------Staff Actions---------------------------\n" +
+                    "-------------------------------------------------------------------\n" +
+                    "                         Choose an option:\n" +
+                    "                         1.View Order Actions\n" +
+                    "                         2.Logout\n" +                         
+                    "---------------------------------------------------------------------\n" +
+                    "\n" +
+                    "Enter your choice (1-4): \n");
+            choice = -1;
+            try {
+                choice = Integer.parseInt(scanner.next());
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input. Please enter a number.");
+                scanner.nextLine(); // Consume the invalid input
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    action = new StaffOrderAction();
+                    action.execute(session, scanner);, scanner);
+                    ScannerProvider.closeScanner();
+                    break;
+                case 2:
+                    loop=false;
+                    System.out.println("Logging out...");
+                    break;
+                default:
+                    System.out.println("Invalid Key! Enter your choice (1-2)");
+                    break;
+                
+            }
+
+        
         ScannerProvider.closeScanner();
     }
 }
