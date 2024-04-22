9d43b2aafee8331203f329285f1659dccb7c12b3
Ian Boo <ianboo.jy@gmail.com>
2024-04-22 14:38:39 +0900

    Update StaffDisplayController.java

--- src/main/java/controllers/StaffDisplayController.java
+++ src/main/java/controllers/StaffDisplayController.java
@@ -7,23 +7,25 @@ import java.util.List;
 import main.java.daos.StaffDAO;
 
 public class StaffDisplayController {
-    private StaffDAO staffDAO;
-    private String branch;
+    private static StaffDAO staffDAO;
+    private static String staffUserID;
 
-    public StaffDisplayController(StaffDAO staffDAO, String branch){
-        this.staffDAO = staffDAO;
-        this.branch = branch;
+    public StaffDisplayController(StaffDAO staffDAO, String staffUserID){
+        StaffDisplayController.staffDAO = staffDAO;
+        StaffDisplayController.staffUserID = staffUserID;
     }
 
-    public void displayStaffList(){  // implement display using STAFF DAO
-        List<Staff> staffList = staffDAO.getElements();
-        // Print the staff details who belong to the 'branch'
-        System.out.println("Staff list for branch: " + branch);
-        for (Staff staff : staffList) {
-            if (staff.getBranch().equals(branch)) {
-                System.out.println(staff);
+    public static void displayStaffList(){  // implement display using STAFF DAO
+        Staff currentUser = staffDAO.findElement(staffUserID);
+        
+        String branch = currentUser.getBranch();
+            List<Staff> staffList = staffDAO.getElements();
+
+            System.out.println("Staff list for branch: " + branch);
+            for (Staff staff : staffList) {
+                if (staff.getBranch().equals(branch)) {
+                    System.out.println(staff); // Assuming Staff has a meaningful toString() implementation
+                }
             }
-        }
     }
-
 }
