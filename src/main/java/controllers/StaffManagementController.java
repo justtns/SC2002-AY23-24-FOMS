b3b3d529ed7a4fa980242c755c4fd89d2dfae056
Jaron <limksjaron123@gmail.com>
2024-04-24 03:14:21 +0800

    staffdisplaycontroller v1.1 - added ver

--- src/main/java/controllers/StaffManagementController.java
+++ src/main/java/controllers/StaffManagementController.java
@@ -1,16 +1,43 @@
 package main.java.controllers;
 
 import main.java.daos.StaffDAO;
-import main.java.utils.types.StaffRole;
 import main.java.models.Staff;
+import main.java.utils.types.StaffRole;
 
+/**
+ * The StaffManagementController class handles the management of staff, including adding, editing, and removing staff records.
+ * It provides methods to perform CRUD (Create, Read, Update, Delete) operations on staff data.
+ * This class is part of a three-layer architecture, serving as the intermediary between the presentation layer (UI)
+ * and the data access layer (DAO).
+ * 
+ * @author SDDA Team 1
+ * @version 1.0
+ * @since 24-Apr-2024
+ */
 public class StaffManagementController {
+    /** The data access object (DAO) for staff */
     private StaffDAO staffDAO;
 
+    /**
+     * Constructs a StaffManagementController object with the specified StaffDAO.
+     * 
+     * @param staffDAO The StaffDAO object to be used by the controller
+     */
     public StaffManagementController(StaffDAO staffDAO){
         this.staffDAO = staffDAO;
     }
 
+    /**
+     * Adds a new staff member to the system.
+     * 
+     * @param name The name of the staff member
+     * @param loginId The login ID of the staff member
+     * @param role The role of the staff member
+     * @param gender The gender of the staff member
+     * @param age The age of the staff member
+     * @param branch The branch where the staff member works
+     * @return True if the staff member is successfully added, false otherwise
+     */
     public boolean addStaff(String name, String loginId,  StaffRole role, String gender, int age, String branch) {
         String defaultPassword = "password";
         Staff newStaff = new Staff(name, loginId, defaultPassword, role, gender, age, branch);
@@ -24,6 +51,15 @@ public class StaffManagementController {
         }
     }
 
+    /**
+     * Edits the details of an existing staff member.
+     * 
+     * @param loginId The login ID of the staff member to be edited
+     * @param newLoginId The new login ID for the staff member
+     * @param newPassword The new password for the staff member
+     * @param newBranch The new branch for the staff member
+     * @return True if the staff member is successfully edited, false otherwise
+     */
     public boolean editStaff(String loginId, String newLoginId, String newPassword, String newBranch) {
         Staff oldStaff = staffDAO.findElement(loginId);
         Staff staff = staffDAO.findElement(loginId);
@@ -40,6 +76,12 @@ public class StaffManagementController {
         }
     }
 
+    /**
+     * Removes an existing staff member from the system.
+     * 
+     * @param loginId The login ID of the staff member to be removed
+     * @return True if the staff member is successfully removed, false otherwise
+     */
     public boolean removeStaff(String loginId) {
         Staff staff = staffDAO.findElement(loginId);
         if(staff != null) {
@@ -51,5 +93,4 @@ public class StaffManagementController {
             return false;
         }
     }
-
 }
