b178c9426cd0d18e1a70767cf718a56050c025d9
justtns <justinloh20@gmail.com>
2024-04-24 04:09:04 +0800

    pushin

--- src/test/java/Applications/CustomerAppRunner.java
+++ src/test/java/Applications/CustomerAppRunner.java
@@ -2,10 +2,11 @@ package test.java.Applications;
 
 
 import main.java.boundaries.CustomerApp;
+import main.java.utils.ScannerProvider;
 
 public class CustomerAppRunner {
     public static void main(String[] args) {
         CustomerApp customerApp = new CustomerApp();
-        customerApp.execute();
+        customerApp.execute(ScannerProvider.getScanner());
     }
 }
