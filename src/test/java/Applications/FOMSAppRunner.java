6a20c19dfd8304f304aed1cf599ed933887b975e
Ian Boo <ianboo.jy@gmail.com>
2024-04-24 04:09:29 +0900

    tired

---
+++ src/test/java/Applications/FOMSAppRunner.java
@@ -0,0 +1,11 @@
+package test.java.Applications;
+
+
+import main.java.boundaries.FOMSApp;
+
+public class FOMSAppRunner {
+    public static void main(String[] args) {
+        FOMSApp fomsApp = new FOMSApp();
+        fomsApp.execute();
+    }
+}
