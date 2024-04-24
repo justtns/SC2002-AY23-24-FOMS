b8889d39f07bf61e1c4c91fc091137f1a80e1f61
Jaron <limksjaron123@gmail.com>
2024-04-24 19:12:13 +0800

    form v1.1 - added javadoc

--- src/main/java/boundaries/Form.java
+++ src/main/java/boundaries/Form.java
@@ -1,5 +1,17 @@
 package main.java.boundaries;
 
+/**
+ * The Form interface represents a generic form in the Fastfood Ordering and Management System (FOMS).
+ * This interface will be implemented by all forms in this project, and the generateForm abstract method will
+ * overridden in all forms that implement this interface.
+ * 
+ * @author SDDA Team 1
+ * @version 1.1
+ * @since 24-Apr-2024
+ */
 public interface Form {
+    /**
+     * Generates the form based on the specific functionality defined in implementing classes.
+     */
     public void generateForm();
 }
