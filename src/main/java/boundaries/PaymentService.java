3a200bc6381dc57143a183d4a912410118e32897
Jaron <limksjaron123@gmail.com>
2024-04-24 20:41:20 +0800

    PaymentService v1.1 - added javadoc

--- src/main/java/boundaries/PaymentService.java
+++ src/main/java/boundaries/PaymentService.java
@@ -2,12 +2,28 @@ package main.java.boundaries;
 
 import java.util.Scanner;
 
+/**
+ * The PaymentService interface defines methods for simulating payment and authenticating payment.
+ * This interface will be implemented by OnlinePaymentService and CreditDebitPaymentService classes.
+ * 
+ * @author SDDA Team 1
+ * @version 1.1
+ * @since 24-Apr-2024
+ */
 public interface PaymentService {
     
-    // payment service simulates payment and authenticates payment 
-    // - since authentification is part of payment process, and payment cannot be made without authentification, it does not violate SRP
-
+    /**
+     * Abstract method to simulates the payment process.
+     * 
+     * @param scanner The Scanner object for user input.
+     * @return true if the payment is successfully simulated, false otherwise.
+     */
     boolean simulatePayment(Scanner scanner);
 
+    /**
+     * Abstrat method to authenticates the payment.
+     * 
+     * @return true if the payment is successfully authenticated, false otherwise.
+     */
     boolean authenticatePayment();
 }
