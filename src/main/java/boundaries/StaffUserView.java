8f8fa7a5c067ef4cfc4d439dde42603f5a2abada
Jaron <limksjaron123@gmail.com>
2024-04-24 20:25:54 +0800

    staffuserview v1.1 - added javadoc

--- src/main/java/boundaries/StaffUserView.java
+++ src/main/java/boundaries/StaffUserView.java
@@ -2,6 +2,19 @@ package main.java.boundaries;
 
 import main.java.utils.loggers.StaffSession;
 
+/**
+ * Interface for defining the behavior of different views for staff, manager and admin roles.
+ * It will be implemented by ManagerView, StaffView and AdminView.
+ *
+ * @author SDDA Team 1
+ * @version 1.1
+ * @since 24-Apr-2024
+ */
 public interface StaffUserView {
+    /**
+     * Executes the view with the provided staff session.
+     * 
+     * @param session The staff session associated with the user.
+     */
     public void execute(StaffSession session);
 }
