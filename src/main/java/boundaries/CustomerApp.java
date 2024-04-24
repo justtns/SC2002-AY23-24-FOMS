ba9ca4ff1e76f19d6f24f372e7d598bf28546b2e
Jaron <limksjaron123@gmail.com>
2024-04-24 21:10:15 +0800

    CustomerApp v1.1 - added javadoc

--- src/main/java/boundaries/CustomerApp.java
+++ src/main/java/boundaries/CustomerApp.java
@@ -6,15 +6,36 @@ import java.util.Scanner;
 
 import main.java.factories.FormFactory;
 
+/**
+ * The CustomerApp class represents the entry point for customer users.
+ * It is a boundary level object that interacts with staff users.
+ * This class provides functionality for customer users to view menu and place an order, make a payment,
+ * check on their orders and logout.
+ * This App is implemented from AppDisplay interface.
+ * 
+ * @author SDDA Team 1
+ * @version 1.1
+ * @since 24-Apr-2024
+ */
+public class CustomerApp implements AppDisplay {
 
-public class CustomerApp implements AppDisplay{
-    public void execute(Scanner scanner){
+    /**
+     * Executes the customer application.
+     * 
+     * This method displays all possible options for customers at a restaurant. They can view menu and place orders,
+     * make payment, or check on order status.
+     * They are directed to the corresponding forms for further actions.
+     * It checks if customer input is valid within 1-4.
+     * 
+     * @param scanner The Scanner object for user input.
+     */
+    public void execute(Scanner scanner) {
         CustomerSession session = new CustomerSession();
         CustomerBranchSelectionForm selectBranch = new CustomerBranchSelectionForm(session, scanner);
         session = selectBranch.generateForm();
         FormFactory actionFactory = new FormFactory();
         Form form;
-        Boolean loop=true;
+        Boolean loop = true;
         while (loop) {
             System.out.println("Order ID:" + session.getOrderId());
             System.out.println("----------------------------------------------------------------------\n" +
@@ -36,15 +57,13 @@ public class CustomerApp implements AppDisplay{
                 System.out.println("Invalid Input. Please enter (1-4)");
                 continue;
             }
-            if (choice < 1 | choice > 4){
+            if (choice < 1 || choice > 4) {
                 System.out.println("Invalid Input. Please enter (1-4)");
-            }
-            else if (choice == 4){
-                loop=false;
+            } else if (choice == 4) {
+                loop = false;
                 System.out.println("Logging out...");
                 break;
-            }
-            else{
+            } else {
                 form = actionFactory.getForm(session, scanner, choice);
                 form.generateForm();
             }
