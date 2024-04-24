0ed07f4a7037ad1da0c6c4438214dbc62fb2e5e8
xndyyy <andychannnn@gmail.com>
2024-04-24 17:04:59 +0800

    updated

--- src/main/java/boundaries/StaffApp.java
+++ src/main/java/boundaries/StaffApp.java
@@ -11,63 +11,73 @@ public class StaffApp implements AppDisplay {
     private StaffSession staffSession;
 
     public void execute(Scanner scanner) {
+        
         System.out.println("----------------------------------------------------------------------\n" +
                            "|--------------------------Login Portal------------------------------|\n" +
                            "----------------------------------------------------------------------\n");
 
         enterRole(scanner);
-
-        System.out.println("Enter username:");
-        String username = scanner.nextLine();
         
-        System.out.println("Enter password:");
-        String password = scanner.nextLine();
+        boolean loop = true;
+        while(loop){
+            System.out.println("Enter username:");
+            String username = scanner.nextLine();
+            
+            System.out.println("Enter password:");
+            String password = scanner.nextLine();
 
-        this.staffLogin = new StaffLogin();
-        
-        boolean usernameAuthenticated = staffLogin.enterUsername(username);
-        boolean passwordAuthenticated = staffLogin.enterPassword(password);
+            this.staffLogin = new StaffLogin();
+            
+            boolean usernameAuthenticated = staffLogin.enterUsername(username);
+            boolean passwordAuthenticated = staffLogin.enterPassword(password);
 
-        if (usernameAuthenticated && passwordAuthenticated) {
-            System.out.println("Login successful for role: " + staffRole + " and user: " + username);
-            this.staffSession = new StaffSession(username, staffRole);
-            StaffUserView view;
-            switch(staffRole){
-                case ADMIN:
-                    view = new AdminView();
-                    view.execute(staffSession);
-                    break;
-                case MANAGER:
-                    view = new ManagerView();
-                    view.execute(staffSession);
-                    break;
-                case STAFF:
-                    view = new StaffView();
-                    view.execute(staffSession);
-                    break;
+            if (usernameAuthenticated && passwordAuthenticated) {
+                System.out.println("Login successful for role: " + staffRole + " and user: " + username);
+                this.staffSession = new StaffSession(username, staffRole);
+                StaffUserView view;
+                switch(staffRole){
+                    case ADMIN:
+                        view = new AdminView();
+                        view.execute(staffSession);
+                        loop = false;
+                        break;
+                    case MANAGER:
+                        view = new ManagerView();
+                        view.execute(staffSession);
+                        loop = false;
+                        break;
+                    case STAFF:
+                        view = new StaffView();
+                        view.execute(staffSession);
+                        loop = false;
+                        break;
+                }
+            } else {
+                System.out.println("Login failed. Please try again.");
             }
-        } else {
-            System.out.println("Login failed. Please try again.");
         }
     }
 
     public void enterRole(Scanner scanner) {
-        System.out.println("Please enter your role (Admin/Manager/Staff): ");
-        String roleInput = scanner.nextLine();
         
-        switch (roleInput.toLowerCase()) {
-            case "admin":
-                this.staffRole = StaffRole.ADMIN;
-                break;
-            case "manager":
-                this.staffRole = StaffRole.MANAGER;
-                break;
-            case "staff":
-                this.staffRole = StaffRole.STAFF;
-                break;
-            default:
-                System.out.println("Invalid role entered.");
-                return;
+        while(true){
+            System.out.println("Please enter your role (Admin/Manager/Staff): ");
+            String roleInput = scanner.nextLine();
+            
+            switch (roleInput.toLowerCase()) {
+                case "admin":
+                    this.staffRole = StaffRole.ADMIN;
+                    return;
+                case "manager":
+                    this.staffRole = StaffRole.MANAGER;
+                    return;
+                case "staff":
+                    this.staffRole = StaffRole.STAFF;
+                    return;
+                default:
+                    System.out.println("Invalid role entered.");
+                    break;
+            }
         }
     }
 }
