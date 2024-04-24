83c4256078aff13aa86015445722f66202320372
Jaron <limksjaron123@gmail.com>
2024-04-24 18:01:20 +0800

    Scannerprovider v1.1 - added javadoc

--- src/main/java/utils/ScannerProvider.java
+++ src/main/java/utils/ScannerProvider.java
@@ -2,13 +2,33 @@ package main.java.utils;
 
 import java.util.Scanner;
 
+/**
+ * Provides a utility class for obtaining a Scanner object.
+ * This class ensures a single Scanner instance is used throughout the application.
+ * It also provides a method to close the Scanner when it is no longer needed.
+ * 
+ * @author SDDA Team 1
+ * @version 1.1
+ * @since 24-Apr-2024
+ */
 public class ScannerProvider {
+    
+    /** The Scanner object used for input operations. */
     private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Retrieves the Scanner object.
+     * 
+     * @return the Scanner object
+     */
     public static Scanner getScanner() {
         return scanner;
     }
 
+    /**
+     * Closes the Scanner object.
+     * It is recommended to call this method when the Scanner is no longer needed.
+     */
     public static void closeScanner() {
         scanner.close();
     }
