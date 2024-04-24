7cc9ad4bbb7c40b72c3285e8f7082cbc38595eb8
Jaron <limksjaron123@gmail.com>
2024-04-24 14:58:23 +0800

    staffrole v1.2 - updated javadoc

--- src/main/java/utils/types/StaffRole.java
+++ src/main/java/utils/types/StaffRole.java
@@ -2,10 +2,11 @@ package main.java.utils.types;
 
 /**
  * Enum representing the roles of staff members.
- * This is used during StaffAuthenticationController and StaffDAO, where the role will be verified against the stored data in Staff Excel.
+ * This is used during StaffApp, StaffAuthenticationController and StaffDAO, 
+ * where the role will be verified against the stored data in Staff Excel.
  *
  * @author SDDA Team 1
- * @version 1.1
+ * @version 1.2
  * @since 24-Apr-2024
  */
 public enum StaffRole {
