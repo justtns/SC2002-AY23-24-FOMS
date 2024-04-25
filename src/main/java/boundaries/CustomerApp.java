cf5f353120d4d7152465f163fb330f3a1bd261e5
Rachel <rachelkjhh@gmail.com>
2024-04-26 00:36:32 +0800

    flush

--- src/main/java/boundaries/CustomerApp.java
+++ src/main/java/boundaries/CustomerApp.java
@@ -70,6 +70,12 @@ public class CustomerApp implements AppDisplay {
                 System.out.println("Invalid Input. Please enter (1-4)");
             } else if (choice == 4) {
                 loop = false;
+                //time delay
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("Logging out...");
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
